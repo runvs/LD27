3db9867ea054ab12079eba0e8e636afdba886e48
Thunraz <thunraz@thunraz.com>
2013-08-24 10:55:09 +0000

    * cTile completed * Ignoring that mysterious 'ipch' folder

--- Code/Project/cTile.cpp
+++ Code/Project/cTile.cpp
@@ -1,14 +1,55 @@
 #include "cTile.h"
 
 
-cTile::cTile()
+cTile::cTile(cTile::eTileType tileType)
 {
-
-}
-
-void cTile::SetPosition(sf::Vector2f vecPos)
-{
-
+	cTile::m_Texture.loadFromFile(cTile::GetFileNameByTileType(tileType));
+	cTile::m_Sprite.setTexture(cTile::m_Texture);
 }
 
 int cTile::s_iTileSizeInPixels = 19;
+
+void cTile::Draw (sf::RenderWindow* renderWindow)
+{
+	renderWindow->draw(cTile::m_Sprite);
+}
+
+// Setter and getter for tile position
+void cTile::SetPosition(sf::Vector2f position)
+{
+	cTile::m_Position = position;
+}
+
+sf::Vector2f cTile::GetPosition()
+{
+	return cTile::m_Position;
+}
+
+// Setter and getter for tile index
+void cTile::SetIndex(sf::Vector2i index)
+{
+	cTile::m_Index = index;
+}
+
+sf::Vector2i cTile::GetIndex()
+{
+	return cTile::m_Index;
+}
+
+std::string cTile::GetFileNameByTileType(cTile::eTileType tileType)
+{
+	switch(tileType)
+	{
+	case GRASS_1:
+		return "gfx/Grass_1.png";
+	case GRASS_2:
+		return "gfx/Grass_2.png";
+	case EARTH_TOP:
+		return "gfx/Earth-Top.png";
+	case EARTH_BELOW:
+		return "gfx/Earth-Below.png";
+	case BLOCK:
+	default:
+		return "gfx/Block.png";
+	}
+}
--- Code/Project/cTile.h
+++ Code/Project/cTile.h
@@ -1,19 +1,35 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <string>
 
 class cTile
 {
 public:
-	cTile();
+	enum eTileType
+	{
+		GRASS_1, GRASS_2, EARTH_TOP, EARTH_BELOW, BLOCK
+	};
 
-	void SetPosition(sf::Vector2f vecPos);
+	cTile(cTile::eTileType tileType);
 
 	static int s_iTileSizeInPixels;
 
-	void Draw ( sf::RenderWindow* RW);
+	void Draw (sf::RenderWindow* renderWindow);
+
+	// Setter and getter for tile position
+	void SetPosition(sf::Vector2f vecPos);
+	sf::Vector2f GetPosition();
+
+	// Setter and getter for tile index
+	void SetIndex(sf::Vector2i vector);
+	sf::Vector2i GetIndex();
 
 private:
 	sf::Texture m_Texture;
 	sf::Sprite m_Sprite;
+	sf::Vector2i m_Index;
+	sf::Vector2f m_Position;
+
+	std::string GetFileNameByTileType(cTile::eTileType tileType);
 };
